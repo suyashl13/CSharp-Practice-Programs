7cab0d8e50f5f7f6131d9a16dff1ab5cc26cc492
Suyash Lawand (Mac) <suyash.lawand@gmail.com>
2024-02-25 12:23:47 +0530

    add: 08-Properties

---
+++ 08-Properties/02-Indexors/Program.cs
@@ -0,0 +1,33 @@
+﻿class Car {
+    private string[] _brandNames = ["BMW", "Skoda", "Honda", "Volkswagen", "Audi"];
+    private String[] _names = ["One", "Two", "Three", "Four", "Five"];
+
+    // Public Indexer
+    public string this[int index] {
+        get {
+            return _brandNames[index];
+        }
+        set {
+            _brandNames[index] = value;
+        }
+    }
+
+    public string this[String index] {
+        get {
+            return _names[Array.IndexOf(_names, index)];
+        }
+        set {
+            this._names[0] = value;
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main() {
+        Car gla8 = new Car();
+        gla8[3] = "Volvo";
+        Console.WriteLine(gla8[3]);
+    }
+}
