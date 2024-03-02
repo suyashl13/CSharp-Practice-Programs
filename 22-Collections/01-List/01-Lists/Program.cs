b6986b402483ab9214483ac91c5d0e6adb5ad813
Suyash Lawand (Mac) <suyash.lawand@gmail.com>
2024-03-02 15:59:57 +0530

    add: 01-List

---
+++ 22-Collections/01-List/01-Lists/Program.cs
@@ -0,0 +1,12 @@
+﻿class Program
+{
+    static void Main() {
+        List<int> numbersList = [10, 20, 30];
+
+        foreach (var item in numbersList)
+        {
+            Console.WriteLine(item);
+        }
+         
+    }
+}
