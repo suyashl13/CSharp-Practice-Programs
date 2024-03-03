24e2f5b2ed56eab8eb8fe0c230b06eb91454b5ce
Suyash Lawand (Mac) <suyash.lawand@gmail.com>
2024-03-03 14:58:11 +0530

    add: dictionaries

--- 22-Collections/01-List/01-Lists/Program.cs
+++ 22-Collections/01-List/01-Lists/Program.cs
@@ -1,12 +1,63 @@
 ﻿class Program
 {
-    static void Main() {
+    static void Main()
+    {
         List<int> numbersList = [10, 20, 30];
 
-        foreach (var item in numbersList)
+        // Add item 
+        numbersList.Add(40);
+
+        // Adding other list
+        List<int> numbersList2 = [50, 60, 70, 60];
+        List<int> numbersList3 = [150, 160, 170, 160];
+
+        // Adding other list
+        numbersList.AddRange(numbersList2);
+
+        // Adding the element at specified index
+        numbersList.Insert(3, 60);
+        numbersList.InsertRange(3, numbersList3);
+
+        // Remove item from spefic index.
+        numbersList.RemoveAt(4);
+
+        // Remove based on condition
+        numbersList.RemoveAll(number => number % 20 != 0); // True means element will be removed. 
+
+        // Removing the element
+        bool isRemoved = numbersList.Remove(60);
+        if (isRemoved)
         {
-            Console.WriteLine(item);
+            Console.WriteLine($"60 is removed from the list");
         }
-         
+
+
+        // IndexOf: returns -1 if not found. and index is returned if found.
+        int index = numbersList.IndexOf(60);
+        Console.WriteLine($"60 found at {index}");
+
+        // Sorting the list
+        numbersList.Sort();
+
+        // Reverse the list
+        numbersList.Reverse();
+
+        // Converting the list to array
+        int[] numbersArray = numbersList.ToArray();
+
+        // For Each Method
+        numbersList.ForEach((number) => Console.WriteLine(number));
+
+        Console.WriteLine(numbersList.Exists(number => number > 55));
+
+
+        // Failed Marks
+        Console.WriteLine(numbersList.Find(marks => marks < 35));
+        
+        // Failed Marks Index
+        Console.WriteLine(numbersList.FindIndex(marks => marks < 35));
+
+        List<String> strings = numbersList.ConvertAll<String>((n) => String.Format("{0}", n));
+        Console.WriteLine(String.Join(", ", strings.ToArray()));
     }
 }
---
+++ 22-Collections/02-Dictionaries/02-Dictionaries/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Dictionaries
+        Dictionary<string, string> states = new Dictionary<string, string>() {
+            {"MH", "Maharashtra"},
+        };
+        states.Add("NY", "New York");
+
+
+        foreach (KeyValuePair<string, string> state in states)
+        {
+            Console.WriteLine($"{state.Key}: {state.Value}");
+        }
+
+        // Get element by index
+        Console.WriteLine("MH:= " + states["MH"]);
+
+        // Removing the element
+        bool isRemoved = states.Remove("NY");
+        if (isRemoved)
+        {
+            Console.WriteLine("Removed NY from states");
+        }
+
+        // Contains key
+        bool isKey = states.ContainsKey("NY");
+
+        // Contains value
+        bool isValue = states.ContainsValue("New York");
+
+        Console.WriteLine(isKey + " " + isValue);
+
+        // Removing all
+        states.Clear();
+
+        // Print all keys;
+        foreach (string key in states.Keys)
+        {
+            Console.WriteLine(key);
+        }
+    }
+}
