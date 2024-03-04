bce869d166280b206282396fff19b646b04cb4b7
suyash.lawand-coditas <suyash.lawand@coditas.com>
2024-03-04 11:34:30 +0530

    add: hashset

---
+++ 22-Collections/05-Hashset/05-Hashset/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program {
+    static void Main() {
+        HashSet<String> employeeNames = [
+            "Suyash",
+            "Bill",
+            "James",
+            "Will",
+            "John",
+            "James B.",
+            "Steve",
+        ];
+
+        // Remove based on values.
+        employeeNames.Remove("Suyash");
+
+        // Remove based on conditions (Has spaces);
+        employeeNames.RemoveWhere(empName => empName.Split(" ").Length > 1);
+
+        // Add
+        employeeNames.Add("Elon Musk");
+
+        // See if contains
+        bool isPresent = employeeNames.Contains("Suyash"); // false as it was removed.
+        Console.WriteLine($"Suyash is in list ? {isPresent}");
+
+        // traverse
+        foreach (String item in employeeNames) {
+            Console.WriteLine($"Item: {item}");
+        }
+
+
+        // Clear
+        employeeNames.Clear();
+    }
+}
