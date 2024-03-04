58d3b8c9772df501253d40552a42ee025e109743
suyash.lawand-coditas <suyash.lawand@coditas.com>
2024-03-04 10:29:03 +0530

    add: collections

---
+++ 22-Collections/03-Sorted-List/03-Sorted-List/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program {
+    static void Main() 
+    {
+        SortedList<int, String> employees = new()  {
+            { 101, "AAA" },
+            {102, "BBB"},
+            {103, "CCC"},
+            {108, "DDD"}
+        };
+
+        // Adding Items in sorted list
+        employees.Add(105, "FFF");
+        employees.Add(107, "HHH");
+
+        // Removing elements from employees.
+        employees.Remove(103);
+
+        foreach (KeyValuePair<int, string> item in employees)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value}");
+        }
+
+        // Check if key contains
+        bool doesExist = employees.ContainsKey(103);
+        String resultMessage = doesExist ? "Found" : "Not Found";
+
+        Console.WriteLine($"Employee with key 103 was {resultMessage}");
+
+        Console.WriteLine($"Found DDD at {employees.IndexOfValue("DDD")}");
+
+
+    }
+}
---
+++ 22-Collections/04-Hashtable/04-Hashtable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+
+class Program
+{
+    static void Main() {
+        Hashtable hashtable = new() {
+            {1001, "AAA"},
+            {1002, "BBB"},
+            {1003, "CCC"},
+            {1004, "DDD"},
+            {1007, "EEE"},
+            {"AAA", 111}
+        };
+
+        bool has1001 = hashtable.Contains(1001);
+        Console.WriteLine(has1001);
+
+        // Adding to hash table
+        hashtable.Add(1006, "HHH");
+        hashtable.Add(1005, "RRR");
+        hashtable.Add("BBB", 2222);
+
+        // Removing from hashtable
+        hashtable.Remove("BBB");
+
+        // Looping through hashtable keys.
+        foreach (var key in hashtable.Keys)
+        {
+            Console.WriteLine($"{key}: {hashtable[key]}");
+        }
+    }
+}
